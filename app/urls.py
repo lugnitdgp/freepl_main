a4b2ef3e80f5592b7331dab514c73f241036773b
Abdur <anvg17@gmail.com>
2016-01-22 00:27:31 +0530

    initialising create_team.html

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^matches/$', views.matches , name='matches'),
     url(r'^match/(?P<id>[0-9]+)$', views.create_team , name='create_team'),
-    # url(r'^rules/$', views.rules , name='rules'),
-    # url(r'^ranking/$', views.ranking , name='ranking'),
+    url(r'^rules/$', views.rules , name='rules'),
+    url(r'^ranking/$', views.ranking , name='ranking'),
 ]
