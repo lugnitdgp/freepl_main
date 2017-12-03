2c0340ea82361662849849bd76d08c405c89dec0
Vivek Modi <nit.vivek@hotmail.com>
2017-12-03 12:38:10 +0530

    corrected google login

--- load.py
+++ load.py
@@ -10,7 +10,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freepl_main.settings')
 #for i in Player.objects.all():
 #    i.delete()
 
-with open('indiaaus.csv') as f:
+with open('indiaeng.csv') as f:
     reader=csv.DictReader(f)
     for row in reader:
         p=(row['Player Name'].strip())
